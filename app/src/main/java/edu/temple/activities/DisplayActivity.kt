f1fca4b98db6b6af33e5dc7b31c4d89b844e282f
Timofei-Gomziakov-TU <tuk36968@temple.edu>
2025-02-18 12:16:24 -0500

    Completed the requirements

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -1,7 +1,9 @@
 package edu.temple.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
 import android.widget.TextView
 
 class DisplayActivity : AppCompatActivity() {
@@ -11,8 +13,11 @@ class DisplayActivity : AppCompatActivity() {
 
         // TODO Step 3: Extract transferred value and use for lyricsDisplayView text size
         with (findViewById<TextView>(R.id.lyricsDisplayTextView)) {
-
+            val intent : Intent = getIntent()
+            val textSize = intent.getFloatExtra("size", 0.0F)
+            setTextSize(TypedValue.COMPLEX_UNIT_SP, textSize.toFloat())
         }
+
 
 
     }
